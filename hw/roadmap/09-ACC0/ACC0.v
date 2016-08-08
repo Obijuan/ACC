369ef91f19ce693d216a977f4b42ff50beb99aa1
obijuan <juan@iearobotics.com>
2016-08-08 19:50:36 +0200

    Instruction register added. Connected to the leds

--- hw/roadmap/09-ACC0/ACC0.v
+++ hw/roadmap/09-ACC0/ACC0.v
@@ -88,14 +88,33 @@ assign clk_in = sw_deb;
 //-- Register Z: Program counter
 reg  [AW-1: 0] Z = BOOT_ADDR;
 
+//-- Control signals for the Z register
+reg inc = 1;
+
 always @(posedge clk_in or posedge rst_in) begin
   if (rst_in==1'b1)
     Z <= BOOT_ADDR;
   else
-    Z <= Z + 1;
+    if (inc)
+      Z <= Z + 1;
 end
 
-assign {d7,d6,d5,d4,d3,d2,d1,d0} = rom_dout[15:8];
+//-- Instruction register
+reg [14:0] RI = 15'b0;
+
+//-- Control signal for the RI register
+//-- Every 15-bit instruction is sotred here before executing
+reg ri_load = 1;
+
+always @(posedge clk)
+  if (ri_load)
+    RI <= rom_dout[14:0];
+
+//-- In ACC0, the 7 more significant bits are shown in leds
+assign {d6,d5,d4,d3,d2,d1,d0} = RI[14:8];
+
+//-- The LED7 is always set to 0
+assign d7 = 1'b0;
 
 endmodule
 
